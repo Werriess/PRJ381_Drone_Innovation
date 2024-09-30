550fd37be7a4792c5cd484e1713a7c91c7dcf630
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-09-30 12:03:40 +0200

    Fix file names
    Change to lower case

--- webapp/src/pages/info.jsx
+++ webapp/src/pages/info.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles.css";
-import Header from "../components/Header";
-import MainContent from "../components/MainContent";
+import Header from "../components/header";
+import MainContent from "../components/maincomponent";
 
 function Info() {
     return(
