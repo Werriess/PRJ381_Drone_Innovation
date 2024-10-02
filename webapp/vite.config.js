b07cb237fde00da982a18a4340d64f435177dc2d
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-10-02 17:22:07 +0200

    webapp: Add docker
    Conduct research and implement docker in webapp
    
    refs: #31

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host:'0.0.0.0',
+    port:5173,
+    watch: {
+      usePolling: true, 
+    },
     proxy: {
       '/register/submit': 'http://localhost:8000',
       '/login': 'http://localhost:8000'
