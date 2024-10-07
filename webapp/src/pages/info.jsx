5f3a27e2ddf89954ec2c8cba922e4227dae42fa7
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-10-07 12:49:34 +0200

    feat(pages): Fix stats page
    Center the page and smooth transition
    
    refs: #36

--- webapp/src/pages/info.jsx
+++ webapp/src/pages/info.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles.css";
-import Header from "../components/header";
+import Header from "../components/Header";
 import MainContent from "../components/maincomponent";
 
 function Info() {
