e5af9a310bf6b4f7bd5cd6a336db32b74619bf2c
Werner Janse van Rensburg <108306252+Werriess@users.noreply.github.com>
2024-10-28 11:31:49 +0200

    feat(specs): Adjust specs page (#49)
    * feat(stats): Implement new stats design
    
    Based on new figma design change stats page(red headers, one left stats block etc)
    
    refs: #36
    
    * feat(specs): Adjust specs page
    
    Problem with specs info block fixed, added info to a table
    
    refs: 48

--- webapp/src/pages/specs.jsx
+++ webapp/src/pages/specs.jsx
@@ -1,81 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
 import Header from "../components/Header";
-import NavMenu from '../components/NavMenu';
+import NavMenu from "../components/NavMenu";
 import LoadingScreen from "../components/loadingScreen";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const Specs = ({children}) => {
-    
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        const loader = new GLTFLoader();
-        
-        loader.load(
-            '../src/assets/DRONE.gltf',
-            (gltf) => {
-                setTimeout(() => {
-                    setIsLoading(false);
-                }, 2000);
-            },
-            undefined,
-            (error) => {
-                console.error('An error occured loading the 3D model:', error);
-                setIsLoading(false);
-            }
-        );
-    }, []);
+const Specs = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const loader = new GLTFLoader();
 
-    return (
-      <div>
-        {isLoading ? (
-          <LoadingScreen />
-        ) : (
-          <>
-            <Header />
-            <section id="mainSpecs">
-              <section id="models">
-                <NavMenu />
-
-                <div id="box">
-                  <div id="root"></div>
-                  {children}
-                </div>
-              </section>
-              <section id="bottom">
-                <div id="specs">
-                  <div>
-                    <h1>Weight:</h1> Approximately 80 g (Propellers and Battery Included)
-                    <br></br>
-                    <h1>Dimensions:</h1> 98×92.5×41 mm 
-                    <br></br><h1>Propeller:</h1> 3 inches{" "}
-                    <br></br><h1>Built-in Functions:</h1> Range Finder, Barometer, LED,
-                    Vision System, 2.4 GHz 802.11n Wi-Fi, 720p Live View{" "}
-                    <br></br>
-                    <h1>Port:</h1> Micro USB Charging Port
-                    <br></br>
-                    <h1>Detachable Battery:</h1> 1.1Ah/3.8V
-                  </div>
-                  <div>
-                  <h1>Max Flight Distance:</h1> 100m<br></br>
-                  <h1> Max Speed:</h1> 8m/s<br></br>
-                  <h1>Max Flight Time:</h1> 13min<br></br>
-                  <h1>Max Flight Height:</h1> 30m
-                  </div>
-                  <div>
-                  <h1>Photo:</h1> 5MP (2592x1936)<br></br>
-                  <h1>FOV:</h1> 82.6°<br></br>
-                  <h1>Video:</h1> HD720P30<br></br>
-                  <h1>Format</h1> JPG(Photo); MP4(Video)<br></br>
-                  <h1>EIS:</h1> Yes
-                  </div>
-                </div>
-              </section>
-            </section>
-          </>
-        )}
-      </div>
+    loader.load(
+      "../src/assets/DRONE.gltf",
+      (gltf) => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      },
+      undefined,
+      (error) => {
+        console.error("An error occured loading the 3D model:", error);
+        setIsLoading(false);
+      }
     );
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <Header />
+          <section id="mainSpecs">
+            <section id="models">
+              <NavMenu />
+              <div id="box">
+                <div id="root"></div>
+                {children}
+              </div>
+              <div id="specs">
+                <div id="specsBlock">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <th>Weight:</th>
+                        <td>
+                          Approximately 80 g (Propellers and Battery Included)
+                        </td>
+                      </tr>
+                      <tr>
+                        <th>Dimensions:</th>
+                        <td>98×92.5×41 mm</td>
+                      </tr>
+                      <tr>
+                        <th>Propeller:</th>
+                        <td>3 inches</td>
+                      </tr>
+                      <tr>
+                        <th>Built-in Functions:</th>
+                        <td>
+                          Range Finder, Barometer, LED, Vision System, 2.4 GHz
+                          802.11n Wi-Fi, 720p Live View
+                        </td>
+                      </tr>
+                      <tr>
+                        <th>Port:</th>
+                        <td>Micro USB Charging Port</td>
+                      </tr>
+                      <tr>
+                        <th>Detachable Battery:</th>
+                        <td>1.1Ah/3.8V</td>
+                      </tr>
+                      <tr>
+                        <th>Max Flight Distance:</th>
+                        <td>100m</td>
+                      </tr>
+                      <tr>
+                        <th>Max Speed:</th>
+                        <td>8m/s</td>
+                      </tr>
+                      <tr>
+                        <th>Max Flight Time:</th>
+                        <td>13min</td>
+                      </tr>
+                      <tr>
+                        <th>Max Flight Height:</th>
+                        <td>30m</td>
+                      </tr>
+                      <tr>
+                        <th>Photo:</th>
+                        <td>5MP (2592x1936)</td>
+                      </tr>
+                      <tr>
+                        <th>FOV:</th>
+                        <td>82.6°</td>
+                      </tr>
+                      <tr>
+                        <th>Video:</th>
+                        <td>HD720P30</td>
+                      </tr>
+                      <tr>
+                        <th>Format:</th>
+                        <td>JPG (Photo); MP4 (Video)</td>
+                      </tr>
+                      <tr>
+                        <th>EIS:</th>
+                        <td>Yes</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </section>
+          </section>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Specs;
