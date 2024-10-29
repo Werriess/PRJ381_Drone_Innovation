5ced8e12f5854d8b84fa09fc0eee35866c4d5d95
Wian Joubert <wianjoubert@outlook.com>
2024-10-29 12:15:12 +0200

    Refactor(database): Schema Changes
    Slightly refactored the DB to allow for referencing of unique drones.
    
    refs: #24

--- webapp/config/db.js
+++ webapp/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const DB_URL = process.env.DB_URL || "mongodb://mongo:mongo@mongodb:27017/Drone?authSource=admin";
+    const DB_URL = process.env.DB_URL;
 
     await mongoose.connect(DB_URL);
     console.log("MongoDB connected successfully");
