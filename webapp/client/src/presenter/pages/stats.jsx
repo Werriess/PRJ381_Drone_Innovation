52346e591a4d028638c5bf28db9bd2e4818f90e1
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-11-13 16:48:27 +0200

    feat(stats): Add general page components
    Add components and based on expedition chosen, create pie chart
    
    refs: #66

--- webapp/client/src/presenter/pages/stats.jsx
+++ webapp/client/src/presenter/pages/stats.jsx
@@ -1,45 +1,15 @@
-import { useState } from "react";
-import generateEmbeddedGoogleMapsLink from "../../../../server/middleware/googleAPI";
-import useFetchDroneData from "../hooks/useDroneData";
-import ExpeditionData from "../features/stats/expeditionData";
-import GoogleMap from "../features/stats/maps";
-import LiveData from "../features/stats/liveData";
-import NavMenu from "../layout/navMenu";
 import Header from "../layout/Header";
-import DroneInfo from "../features/stats/droneInfo";
-import PieChartSection from "../features/stats/pieChartBlock";
 import StatsNavMenu from "../layout/statsNavMenu";
+import StatsBlock from "../features/stats/general/statsBlock";
 
 const Stats = () => {
-  const [droneID, setDroneID] = useState("");
-  const { droneData, expeditionData } = useFetchDroneData(droneID);
-
-  const mapUrl =
-    expeditionData.length > 0 && expeditionData[0].location
-      ? generateEmbeddedGoogleMapsLink(
-          expeditionData[0].location.latitude,
-          expeditionData[0].location.longitude
-        )
-      : "";
-
   return (
     <div id="stats">
       <Header />
       <section id="statsContent">
         <StatsNavMenu />
         <div id="statsContentRight">
-          <div id="statsInnerLeft">
-            <DroneInfo
-              droneID={droneID}
-              setDroneID={setDroneID}
-              droneData={droneData}
-            />
-            <ExpeditionData expeditionData={expeditionData} />
-            <PieChartSection />
-          </div>
-          <div id="statsInnerRight">
-          
-          </div>
+          <StatsBlock/>
         </div>
       </section>
     </div>
