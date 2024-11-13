abf685eceb6fd0535121b6511a6c02867a1487f9
Werner Janse van Rensburg <108306252+Werriess@users.noreply.github.com>
2024-11-13 21:00:26 +0200

    feat(stats): Add live data content (#70)
    Add live data content block using MUI
    
    refs: #66

--- webapp/client/src/presenter/features/stats/liveData/liveData.jsx
+++ webapp/client/src/presenter/features/stats/liveData/liveData.jsx
@@ -1,9 +1,19 @@
+import { Box, Card } from "@mui/material";
+import LiveDataCard from "./components/liveDataCard";
+
 const LiveData = () => {
-    return (
-        <div>
-            <p>Test</p>
-        </div>
-    )
-}
+  return (
+      <Box sx={{ minWidth: 275}}>
+        <Card variant="outlined" sx={{p: 10, mr: 5}}>
+          {
+            <LiveDataCard
+              title="Live Data:"
+              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum illum dolores, quidem cum exercitationem assumenda consequuntur non saepe quis natus ad, delectus perspiciatis? Aspernatur, non. Quo perspiciatis reiciendis quasi esse! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum illum dolores, quidem cum exercitationem assumenda consequuntur non saepe quis natus ad, delectus perspiciatis? Aspernatur, non. Quo perspiciatis reiciendis quasi esse!"
+            />
+          }
+        </Card>
+      </Box>
+  );
+};
 
 export default LiveData;
