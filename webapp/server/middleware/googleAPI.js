2cfbede8914b6fe6796f81f54663c2e1b0048d60
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-11-14 14:25:04 +0200

    fix(maps): Remove googlemaps component

--- webapp/server/middleware/googleAPI.js
+++
@@ -1,5 +0,0 @@
-function generateEmbeddedGoogleMapsLink(latitude, longitude) {
-    return `https://www.google.com/maps/embed/v1/view?key=AIzaSyBipMP0V5BocGHOIhzGj-tJbccf7mru-es&center=${latitude},${longitude}&zoom=14&maptype=satellite`;
-}
-
-export default generateEmbeddedGoogleMapsLink;
